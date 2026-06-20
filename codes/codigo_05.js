5049c4e805cdb1ab91fc974f0ab59746d7e89877
Diana Gomez <digoal2022@gmail.com>
2026-06-19 19:03:46 -0600

    Avance home pokedex

--- codes/codigo_05.js
+++ codes/codigo_05.js
@@ -1,57 +1,109 @@
 /*jshint sub:true*/
+
 // Declaración de variables locales
 // relacionadas con interface html
-var btnGuardar = document.getElementById("btnGuardar");
-var btnMostrar = document.getElementById("btnMostrar");
-var txtNomb = document.getElementById("txtNomb");
-var txtEmail = document.getElementById("txtEmail");
-var txtContra = document.getElementById("txtContra");
+var cmbGeneracion = document.getElementById("cmbGeneracion");
 var resultados = document.getElementById("Datos");
 
-var salida = "";
-//Declara las variables para conectarse con servidor remoto
-//que contiene el web service
+// Declara las variables para conectarse con el servicio remoto
+// que contiene la informacion de los Pokemon (PokeAPI)
 //--------------------------------------------------------------
-var remoto = new XMLHttpRequest();
-var url = "http://demoyork.com:5000";
+var url = "https://pokeapi.co/api/v2/pokemon";
+var urlImagenes = "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/";
 
-//Programación de evento botón guardar
-btnGuardar.addEventListener("click",function(){
+//--------------------------------------------------------------
+// Función que carga el Pokédex según la generación seleccionada
+// en el combo cmbGeneracion (offset y limit van en el "value"
+// de cada <option>, ej: "151,100")
+//--------------------------------------------------------------
+function cargarGeneracion(){
+
+    var valores = cmbGeneracion.value.split(",");
+    var offset = valores[0];
+    var limit = valores[1];
+
+    resultados.innerHTML = "<p class='cargando'>Cargando Pokémon...</p>";
+
     //Determina la funcion HTTPRequest entre sitio local y el remoto
-    remoto.open("POST",url+"/signup",true);
+    var remoto = new XMLHttpRequest();
+
+    remoto.open("GET", url + "?offset=" + offset + "&limit=" + limit, true);
 
     //Determina la forma de intercambio de datos entre el sitio local
-    //el sitio remoto para la pagina actual
-    remoto.setRequestHeader('Accept', 'application/json');
-    remoto.setRequestHeader("Content-Type","application/json");
+    //y el sitio remoto para la pagina actual
+    remoto.setRequestHeader("Accept","application/json");
 
     remoto.onreadystatechange = function (){
-        if(remoto.readyState==4){
-            if(remoto.status == 201){
-                salida =  "<br /><br />";
+        if(remoto.readyState == 4){
+            if(remoto.status == 200){
                 var resul = JSON.parse(remoto.responseText);
-
-                salida = salida.concat('status code: '    + resul.status_code    + '<br />');
-                salida = salida.concat('status message: ' + resul.status_message + '<br />');
-
-                salida = salida.concat('Datos Registrados<br />------------------------<br />');
-
-                var data = resul.data["user"];
-
-                salida = salida.concat('Token: '  + data["token"]  + '<br />');
-                salida = salida.concat('Nombre: ' + data["name"]   + '<br />');
-                salida = salida.concat('eMail: '  + data["email"]  + '<br />');
-                salida = salida.concat('Clave: '  + data["passwd"] + '<br />');
-
-                document.getElementById("Datos").innerHTML = salida;
+                dibujarTarjetas(resul.results);
             }else{
-                document.getElementById("Datos").innerHTML = (remoto.responseText);
-            } //fin del if de status
-        }//fin del if readyState
-    }//fin de la funcion interna
+                resultados.innerHTML = "<p>Error al cargar los datos: " + remoto.status + "</p>";
+            }
+        }//fin del if de readyState
+    };//fin de la funcion interna
 
-    var datos = JSON.stringify({"name":txtNomb.value,
-                                "email":txtEmail.value,
-                                "passwd":txtContra.value});
-    remoto.send(datos);
-});
+    remoto.send();
+}
+
+//--------------------------------------------------------------
+// Dibuja una tarjeta (imagen + nombre) por cada Pokémon recibido
+// y le asigna el evento click para pedir el detalle
+//--------------------------------------------------------------
+function dibujarTarjetas(lista){
+
+    var salida = "";
+
+    for(var i = 0; i < lista.length; i++){
+
+        var nombre = lista[i]["name"];
+
+        // El "url" de cada Pokemon trae su numero al final,
+        // ej: https://pokeapi.co/api/v2/pokemon/6/
+        var partes = lista[i]["url"].split("/");
+        var idPokemon = partes[partes.length - 2];
+        var idFormato = ("000" + idPokemon).slice(-3);
+        var imagen = urlImagenes + idFormato + ".png";
+
+        salida = salida.concat(
+            '<div class="tarjeta" data-nombre="' + nombre + '">' +
+                '<img src="' + imagen + '" alt="' + nombre + '">' +
+                '<p>' + nombre + '</p>' +
+            '</div>'
+        );
+    }//fin del for
+
+    resultados.innerHTML = salida;
+
+    // Asigna el evento click a cada tarjeta recien generada
+    var tarjetas = document.getElementsByClassName("tarjeta");
+    for(var j = 0; j < tarjetas.length; j++){
+        tarjetas[j].addEventListener("click",function(){
+            var nombreSel = this.getAttribute("data-nombre");
+            mostrarDetalle(nombreSel);
+        });
+    }
+}
+
+//--------------------------------------------------------------
+// IMPORTANTE: mostrarDetalle(nombrePokemon) y cerrarModal() las
+// hace mi compañera (Persona 2). Dejo este "stub" temporal SOLO
+// para poder probar mi parte sin que el código truene mientras
+// ella termina su función real; cuando ella agregue su función
+// en este mismo archivo, esta de abajo debe eliminarse.
+//--------------------------------------------------------------
+if(typeof mostrarDetalle !== "function"){
+    function mostrarDetalle(nombrePokemon){
+        console.log("Pendiente: falta implementar mostrarDetalle() -> " + nombrePokemon);
+    }
+}
+
+//--------------------------------------------------------------
+// Programación de eventos
+//--------------------------------------------------------------
+cmbGeneracion.addEventListener("change", cargarGeneracion);
+
+
+//--------------------------------------------------------------
+cargarGeneracion();
