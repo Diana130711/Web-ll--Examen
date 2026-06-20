487e91670f5194beb867744bda687d048cb11249
saCastillo1 <sacastillope@est.utn.ac.cr>
2026-06-19 20:02:52 -0600

    Detalles finales

--- codes/codigo_05.js
+++ codes/codigo_05.js
@@ -1,10 +1,7 @@
-
 var cmbGeneracion = document.getElementById("cmbGeneracion");
 var resultados = document.getElementById("Datos");
 
 var url = "https://pokeapi.co/api/v2/pokemon";
-var urlImagenes = "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/";
-
 
 function cargarGeneracion(){
 
@@ -14,20 +11,22 @@ function cargarGeneracion(){
 
     resultados.innerHTML = "<p class='cargando'>Cargando Pokémon...</p>";
 
-   
     var remoto = new XMLHttpRequest();
 
     remoto.open("GET", url + "?offset=" + offset + "&limit=" + limit, true);
-
-    
     remoto.setRequestHeader("Accept","application/json");
 
-    remoto.onreadystatechange = function (){
+    remoto.onreadystatechange = function(){
+
         if(remoto.readyState == 4){
+
             if(remoto.status == 200){
+
                 var resul = JSON.parse(remoto.responseText);
                 dibujarTarjetas(resul.results);
+
             }else{
+
                 resultados.innerHTML = "<p>Error al cargar los datos: " + remoto.status + "</p>";
             }
         }
@@ -36,53 +35,42 @@ function cargarGeneracion(){
     remoto.send();
 }
 
-
 function dibujarTarjetas(lista){
 
     var salida = "";
 
-    for(var i = 0; i < lista.length; i++){
+   for(var i = 0; i < lista.length; i++){
 
-        var nombre = lista[i]["name"];
+    var nombre = lista[i]["name"];
 
-        
-        var partes = lista[i]["url"].split("/");
-        var idPokemon = partes[partes.length - 2];
-        var idFormato = ("000" + idPokemon).slice(-3);
-        var imagen = urlImagenes + idFormato + ".png";
+    var partes = lista[i]["url"].split("/");
+    var idPokemon = partes[partes.length - 2];
 
-        salida = salida.concat(
-            '<div class="tarjeta" data-nombre="' + nombre + '">' +
-                '<img src="' + imagen + '" alt="' + nombre + '">' +
-                '<p>' + nombre + '</p>' +
-            '</div>'
-        );
-    }
+    var imagen =
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
+        + idPokemon + ".png";
+
+    salida = salida.concat(
+        '<div class="tarjeta" data-nombre="' + nombre + '">' +
+            '<img src="' + imagen + '" alt="' + nombre + '">' +
+            '<p>' + nombre + '</p>' +
+        '</div>'
+    );
+}
+
     resultados.innerHTML = salida;
 
-  
     var tarjetas = document.getElementsByClassName("tarjeta");
+
     for(var j = 0; j < tarjetas.length; j++){
-        tarjetas[j].addEventListener("click",function(){
+
+        tarjetas[j].addEventListener("click", function(){
+
             var nombreSel = this.getAttribute("data-nombre");
             mostrarDetalle(nombreSel);
         });
     }
 }
-
-
-if(typeof mostrarDetalle !== "function"){
-    function mostrarDetalle(nombrePokemon){
-        console.log("Pendiente: falta implementar mostrarDetalle() -> " + nombrePokemon);
-    }
-}
-
-
-cmbGeneracion.addEventListener("change", cargarGeneracion);
-
-
-
-cargarGeneracion();
 
 function mostrarDetalle(nombrePokemon){
 
@@ -90,6 +78,7 @@ function mostrarDetalle(nombrePokemon){
     var urlDetalle = "https://pokeapi.co/api/v2/pokemon/" + nombrePokemon;
 
     remoto.open("GET", urlDetalle, true);
+    remoto.setRequestHeader("Accept","application/json");
 
     remoto.onreadystatechange = function(){
 
@@ -100,43 +89,74 @@ function mostrarDetalle(nombrePokemon){
                 var pokemon = JSON.parse(remoto.responseText);
 
                 var numero = pokemon.id.toString().padStart(3,"0");
-                var imagen = "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/" + numero + ".png";
+
+                var imagen = "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/"
+                           + numero + ".png";
 
                 var tipos = "";
                 var habilidades = "";
                 var movimientos = "";
 
-                for(var i=0; i<pokemon.types.length; i++){
-                    tipos += pokemon.types[i].type.name + " ";
+                for(var i = 0; i < pokemon.types.length; i++){
+                    tipos = tipos.concat(pokemon.types[i].type.name);
+
+                    if(i < pokemon.types.length - 1){
+                        tipos = tipos.concat(" - ");
+                    }
                 }
 
-                for(var j=0; j<pokemon.abilities.length; j++){
-                    habilidades += pokemon.abilities[j].ability.name + " ";
+                for(var j = 0; j < pokemon.abilities.length; j++){
+                    habilidades = habilidades.concat(pokemon.abilities[j].ability.name);
+
+                    if(j < pokemon.abilities.length - 1){
+                        habilidades = habilidades.concat(" - ");
+                    }
                 }
 
-                for(var k=0; k<10 && k<pokemon.moves.length; k++){
-                    movimientos += pokemon.moves[k].move.name + " - ";
+                for(var k = 0; k < 10 && k < pokemon.moves.length; k++){
+                    movimientos = movimientos.concat(pokemon.moves[k].move.name);
+
+                    if(k < 9 && k < pokemon.moves.length - 1){
+                        movimientos = movimientos.concat(" - ");
+                    }
                 }
 
                 var salida = "";
 
-                salida += '<div class="modal-contenido">';
-                salida += '<button onclick="cerrarModal()" class="btn-cerrar">X</button>';
-                salida += '<h2>' + pokemon.name + '</h2>';
-                salida += '<div class="modal-cuerpo">';
-                salida += '<div class="modal-img"><img src="' + imagen + '" alt="' + pokemon.name + '"></div>';
-                salida += '<div class="modal-info">';
-                salida += '<p><b>Pokémon ID:</b> #' + numero + '</p>';
-                salida += '<p><b>Weight:</b> ' + (pokemon.weight/10) + ' kgs</p>';
-                salida += '<p><b>Height:</b> ' + (pokemon.height/10) + ' mts</p>';
-                salida += '<p><b>Types:</b> ' + tipos + '</p>';
-                salida += '<p><b>Abilities:</b> ' + habilidades + '</p>';
-                salida += '<p><b>Moves:</b> ' + movimientos + '</p>';
-                salida += '</div>';
-                salida += '</div>';
-                salida += '</div>';
+                salida = salida.concat('<div class="modal-contenido">');
+                salida = salida.concat('<button onclick="cerrarModal()" class="btn-cerrar">X</button>');
+
+                salida = salida.concat('<h2>' + pokemon.name + '</h2>');
+
+                salida = salida.concat('<div class="modal-cuerpo">');
+
+                salida = salida.concat('<div class="modal-img">');
+                salida = salida.concat('<img src="' + imagen + '" alt="' + pokemon.name + '">');
+                salida = salida.concat('</div>');
+
+                salida = salida.concat('<div class="modal-info">');
+                salida = salida.concat('<p><b>Pokémon ID:</b> #' + numero + '</p>');
+                salida = salida.concat('<p><b>Weight:</b> ' + (pokemon.weight / 10) + ' kgs</p>');
+                salida = salida.concat('<p><b>Height:</b> ' + (pokemon.height / 10) + ' mts</p>');
+                salida = salida.concat('<p><b>Types:</b> ' + tipos + '</p>');
+                salida = salida.concat('<p><b>Abilities:</b> ' + habilidades + '</p>');
+                salida = salida.concat('<p><b>Moves:</b> ' + movimientos + '</p>');
+                salida = salida.concat('</div>');
+
+                salida = salida.concat('</div>');
+                salida = salida.concat('</div>');
 
                 document.getElementById("modalPokemon").innerHTML = salida;
+                document.getElementById("modalPokemon").classList.remove("oculto");
+
+            }else{
+
+                document.getElementById("modalPokemon").innerHTML =
+                    '<div class="modal-contenido">' +
+                    '<button onclick="cerrarModal()" class="btn-cerrar">X</button>' +
+                    '<p>Error al cargar el detalle del Pokémon.</p>' +
+                    '</div>';
+
                 document.getElementById("modalPokemon").classList.remove("oculto");
             }
         }
@@ -146,5 +166,10 @@ function mostrarDetalle(nombrePokemon){
 }
 
 function cerrarModal(){
+
     document.getElementById("modalPokemon").classList.add("oculto");
 }
+
+cmbGeneracion.addEventListener("change", cargarGeneracion);
+
+cargarGeneracion();
